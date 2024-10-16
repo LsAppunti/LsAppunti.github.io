2497d3bf64d1dd46d03fdd2b01aaaeee91becf2e
LsAppunti <lsappunti@gmail.com>
2024-10-16 19:54:40 +0200

    fix#1 for switch camera icon

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ebe2795611570ba1e96cc3feef1a171e",
+const RESOURCES = {"flutter_bootstrap.js": "975baf0d3dfcad1d860a84332e1a5c5e",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "a9119c3de45227f083e3882b5e363312",
+"main.dart.js": "be801f30b9dd68845786788fe4ff6587",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
