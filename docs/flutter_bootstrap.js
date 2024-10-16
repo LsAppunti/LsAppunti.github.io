42966e26f80fe93bd2afee8479c3f9e74d735d31
LsAppunti <lsappunti@gmail.com>
2024-10-16 18:37:32 +0200

    enabled switch camera function

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2777451382"
+    serviceWorkerVersion: "117052428"
   }
 });
