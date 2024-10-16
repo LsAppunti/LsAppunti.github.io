93cc7327af5b36bfba478aaaaa0aeab71ee405c3
LsAppunti <lsappunti@gmail.com>
2024-10-16 18:54:37 +0200

    mirror on x axis for camera

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ed840ebe98d41600731cd9b58b9dfb0b",
+const RESOURCES = {"flutter_bootstrap.js": "ebe2795611570ba1e96cc3feef1a171e",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "a91fbebcf8c123e918f494f21b46c92c",
+"main.dart.js": "a9119c3de45227f083e3882b5e363312",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
