ef4ceddf894517b11e8f396d96a25e312f106e89
LsAppunti <lsappunti@gmail.com>
2024-10-17 20:13:21 +0200

    x-axis flip for send photo

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "aaac6d4683d92f29951db0ef94b18488",
+const RESOURCES = {"flutter_bootstrap.js": "ae2417631d88ace6142b0333e442379c",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "01476d8284b06001007c5ae95ff9e0b1",
+"main.dart.js": "ba485dd7c1c77d3eeaa5bcd7e04e414b",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
