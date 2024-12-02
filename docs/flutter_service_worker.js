6fa46ed75549dfb8fbb59f24b85d2c1b33bcd622
LsAppunti <lsappunti@gmail.com>
2024-12-02 21:11:18 +0100

    Remove of 'Presentazione Liste' banner

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a2a5b3c82ea4971ef687ec70131b4593",
+const RESOURCES = {"flutter_bootstrap.js": "0c2525af18026953a15eff3358d87251",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "61549b84c709aedfb5f01058f6044099",
+"main.dart.js": "f84e3e91a0f38ec2dc4e7f7fc9c54e8f",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
