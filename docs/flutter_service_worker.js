872a07933ca9a97928c7cd9998d236b76abc7fca
LsAppunti <lsappunti@gmail.com>
2024-12-04 20:40:32 +0100

    Added of firebase_auth, auth_service.dart file. Start to work in access_page.dart

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "66bc99b8681c1dc0a5504992571dfabd",
+const RESOURCES = {"flutter_bootstrap.js": "13932dd0c33a2e316e5cd24b11dd1558",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "83100285a540bcd016c3eb3fea69aa2e",
+"main.dart.js": "ddd9fa0a31de2d113669a60de0f9bc1a",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
@@ -16,7 +16,7 @@ const RESOURCES = {"flutter_bootstrap.js": "66bc99b8681c1dc0a5504992571dfabd",
 "icons/README.txt": "d3df3991a31f034bfa98afdfa3c622e1",
 "manifest.json": "701e4f7ce743a83f8ddcaff3479cbc37",
 "assets/AssetManifest.json": "bd8f22190b1e175ae7278c7f5ec8cb98",
-"assets/NOTICES": "58ea69ddc0a9db4368dec07e41d4abb1",
+"assets/NOTICES": "cef3de8b14f95fd15120cba511a63385",
 "assets/FontManifest.json": "1ba75d0544375ffcfebffc16fc7e9764",
 "assets/AssetManifest.bin.json": "61e87af460a50eb55e2704e2df456343",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
