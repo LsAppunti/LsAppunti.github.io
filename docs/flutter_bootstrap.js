e6189a2354fddc18c539b5023ee5b69c09c2c0f3
LsAppunti <lsappunti@gmail.com>
2024-12-05 21:45:36 +0100

    Added the original _login function after the AuthService.signUp

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3615143182"
+    serviceWorkerVersion: "2499922162"
   }
 });
