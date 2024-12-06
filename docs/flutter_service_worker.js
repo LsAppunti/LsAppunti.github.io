024100e25c238aeb9e13ed6aa0514878aea16798
LsAppunti <lsappunti@gmail.com>
2024-12-06 17:38:12 +0100

    Solving the profile picture

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "112df42ca4d483100ac10c144b3983c8",
+const RESOURCES = {"flutter_bootstrap.js": "3e705b66fa8492fa3b42da5eb3db4f8b",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "04360b86c45b1fe64ffd883536b3295e",
+"main.dart.js": "582feefc8e260f9056185ed02531872c",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
