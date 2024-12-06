1c8a40612b6115f75a6ea9aabace9256f7e9a9ea
LsAppunti <lsappunti@gmail.com>
2024-12-06 20:05:33 +0100

    Code change in the access_button.dart

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "442ac5eb96380d3ef56ca1c3b9fff27d",
+const RESOURCES = {"flutter_bootstrap.js": "2a2e61c97570d28ce811650091a6c5ef",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "07cf7ac722312799c48f6868bc1f7445",
+"main.dart.js": "55edc6e934f52ee0fa3c70810d845b4f",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
