1cfdc02a5d3f2797f83ebd79b07ae07910e54f29
LsAppunti <lsappunti@gmail.com>
2024-12-06 17:15:22 +0100

    Trying to fix the profile picture image

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9f3c7871a6aea21b0075956ec296935a",
+const RESOURCES = {"flutter_bootstrap.js": "032d33af78edf15a83d6a7468c53521e",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "83bc862b56aa62ef5ddbdf5ab9b2b2ed",
+"main.dart.js": "bbd3a66b8460b009b8063403bc5c3d98",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "9f3c7871a6aea21b0075956ec296935a",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "9eceb8f21231f7ce7d1c9d9b36afae8d",
-"assets/fonts/MaterialIcons-Regular.otf": "17b75f45038b331621f2951325947f16",
+"assets/fonts/MaterialIcons-Regular.otf": "53a0f834b1fcc2d368b7f040585803cd",
 "assets/assets/svg/Logo.svg": "de23c8f845ebbaf7c6e621286c5a74d9",
 "assets/assets/svg/LSAppunti.svg": "1fe3bde94a6dd875c6c85c79cc4fca05",
 "assets/assets/images/CaptureCamera.png": "dd839b44cb3d3cc4a4c21b9be42a9bfa",
