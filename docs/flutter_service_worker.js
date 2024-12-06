f7dd0c32cbb559e0ed00e6649c139cb8b868a87e
LsAppunti <lsappunti@gmail.com>
2024-12-06 17:05:36 +0100

    Trying to pass the profile picture

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "fbb90c054ce784607504c1a0eec543d3",
+const RESOURCES = {"flutter_bootstrap.js": "a1fb30bdaf838ca44b65e9ab57bbc836",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "79a050c321e8280a64b2a82c8bfba193",
+"main.dart.js": "ee0252e322a2247975cf4387d80a07cd",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
