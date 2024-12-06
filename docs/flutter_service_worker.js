96e12c73b4deda6ce955a26b879ed0542525b50c
LsAppunti <lsappunti@gmail.com>
2024-12-06 16:44:11 +0100

    Add of navigation for user_page

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "88ab68a502d0a41567a19914b7c72af1",
+const RESOURCES = {"flutter_bootstrap.js": "714d6367826234510ca1136b20100232",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "222cfe216e2926937d2ede9195776530",
+"main.dart.js": "ae2477fea4dead221b594bcec428dcaf",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
