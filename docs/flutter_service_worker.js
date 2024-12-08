f48b5a5e54d24baf463fdcf044a2a342153a4782
LsAppunti <lsappunti@gmail.com>
2024-12-08 22:44:26 +0100

    auto close for the slidable

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "60f6e0dbfe07e4777e0daf1b0aa9ad0d",
+const RESOURCES = {"flutter_bootstrap.js": "fe892589c9cd4b256eb29920790b8d55",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "815d832683561fd8f0afc66d55b1d962",
+"main.dart.js": "14be101fc9bb6f9691f28b4a3577c30d",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
