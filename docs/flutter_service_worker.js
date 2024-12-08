e502e808eb7f67cd542d4bdf18d6268f210b90ae
LsAppunti <lsappunti@gmail.com>
2024-12-08 20:51:11 +0100

    Tried something

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "29913ad79104da95a98995d36a5a973e",
+const RESOURCES = {"flutter_bootstrap.js": "285a6bb0ffe791daf274cafa9d67ce8e",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "03b36ca957851b4a753b653889dc5207",
+"main.dart.js": "a99402bf6912411911eabd634febe3fc",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
