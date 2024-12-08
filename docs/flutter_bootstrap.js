8db772f83ea172f9f0633ba9289407227b64bfd1
LsAppunti <lsappunti@gmail.com>
2024-12-08 19:11:19 +0100

    Trying to solve the 'null check operator on a null value'

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4073803538"
+    serviceWorkerVersion: "3894432169"
   }
 });
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "fadcea4bb5711614ceab04a9812633d0",
+const RESOURCES = {"flutter_bootstrap.js": "4640ceb063951d67511e83381c1ea3cb",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "3c546ad4e841f7d3319877de45bb1f99",
+"main.dart.js": "e75e5b6bf7d2c9d22a782d87901cdd05",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
