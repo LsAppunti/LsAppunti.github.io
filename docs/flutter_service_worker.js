52f462c05967b3ba62cb7a13e0d5e06e3e6913ef
LsAppunti <lsappunti@gmail.com>
2024-12-08 23:07:30 +0100

    Added the pdf info page

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "62a6946b4678bb2cec5687bc5c009363",
+const RESOURCES = {"flutter_bootstrap.js": "3ad40585a43f4a86efc4560e773c27f0",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "4d49b5b0d91ba35c8a8d69db663f0b62",
+"main.dart.js": "946ebf82d2898ee4130b8137e047d5bf",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "62a6946b4678bb2cec5687bc5c009363",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "cec134d7efb816f5fe618970a9ca6a69",
-"assets/fonts/MaterialIcons-Regular.otf": "b15458d121f4ba4c1ff5e976e0585561",
+"assets/fonts/MaterialIcons-Regular.otf": "dd59de415c0475a67d270487bcd807ad",
 "assets/assets/svg/Logo.svg": "de23c8f845ebbaf7c6e621286c5a74d9",
 "assets/assets/svg/LSAppunti.svg": "1fe3bde94a6dd875c6c85c79cc4fca05",
 "assets/assets/images/CaptureCamera.png": "dd839b44cb3d3cc4a4c21b9be42a9bfa",
