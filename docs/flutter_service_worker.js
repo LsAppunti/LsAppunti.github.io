3ca9a72ade7363a06049b4a367f049d72a316eaa
LsAppunti <lsappunti@gmail.com>
2024-12-08 19:01:02 +0100

    Logout implement on the 'Esci' button

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "38151fb8c602a816ec53033b9c992a08",
+const RESOURCES = {"flutter_bootstrap.js": "fadcea4bb5711614ceab04a9812633d0",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "90d4cca0572842926ea6627128eaf7c6",
+"main.dart.js": "3c546ad4e841f7d3319877de45bb1f99",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
