7218935deb8bbc315641ec2ec9fbbe3e9ae5ec6c
LsAppunti <lsappunti@gmail.com>
2024-12-08 23:15:27 +0100

    Minor UI changes

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3ad40585a43f4a86efc4560e773c27f0",
+const RESOURCES = {"flutter_bootstrap.js": "d21167d31f2f1d92fe9bc12f64f487b9",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "946ebf82d2898ee4130b8137e047d5bf",
+"main.dart.js": "98c37518f365858c4bfde1a96c817016",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
