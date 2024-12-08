90fef675ca9c32148873e2c0f1b7403cc96280f3
LsAppunti <lsappunti@gmail.com>
2024-12-08 18:34:11 +0100

    Implement the singOut and regex

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e742ef42010a06b97f50dd69c7c5c898",
+const RESOURCES = {"flutter_bootstrap.js": "0fb3713289fd5b090e2af81707a0558c",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "0724201271dfd173066b0b4baf99de92",
+"main.dart.js": "982419650869dbfe88ea736ca8b98b07",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
