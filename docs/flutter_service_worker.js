1da48af805eadbfe3cb630b8951cc9435c0cdc7b
LsAppunti <lsappunti@gmail.com>
2024-12-29 17:39:11 +0100

    fix #17

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "4f88e637224917d39b3e8c8c1e055861",
+const RESOURCES = {"flutter_bootstrap.js": "fd24b590e77373c4e712fb0c6940be94",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "963925e5ae24b99c4c8a1a3abc2007c0",
+"main.dart.js": "fdc81b233511026df8fc266efa957ad8",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
