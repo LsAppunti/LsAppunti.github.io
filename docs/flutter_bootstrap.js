1ec16c934f169f4541bf1e8e253639c1abc067b3
LsAppunti <lsappunti@gmail.com>
2024-12-31 18:11:59 +0100

    Fix #1 for the wrong mail bug

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3850631454"
+    serviceWorkerVersion: "1094601262"
   }
 });
