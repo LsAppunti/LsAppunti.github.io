7a309a83da4b36f7915a293501eae199256cb024
LsAppunti <lsappunti@gmail.com>
2025-01-02 13:38:44 +0100

    Remove of ugly ass container

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ce73f293c480c7377124787051b9d748",
+const RESOURCES = {"flutter_bootstrap.js": "9672b1c6bf3d34db1b02cc3d17de04e1",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "119fd8eb990a6a3ec3dabdd27a49d6cb",
+"main.dart.js": "250e653fb90faf984b797284628975bd",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
