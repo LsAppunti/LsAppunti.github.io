163f161baf2e4d78bd613723c81fc0c8e12ccd9b
LsAppunti <lsappunti@gmail.com>
2025-01-02 12:35:54 +0100

    Back to old code, to test

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4288827530"
+    serviceWorkerVersion: "2367902589"
   }
 });
