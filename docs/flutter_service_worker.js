c22b31b1c7b0c0df3c48be91b7be655d12f80600
LsAppunti <lsappunti@gmail.com>
2025-01-02 13:29:08 +0100

    Update to only display the error banner

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b1e5d4bd888003767027132b4d270176",
+const RESOURCES = {"flutter_bootstrap.js": "3c999b0d451f6592369cdf20ec2c87a3",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "52c71d2916127425e73339c4cc1610e2",
+"main.dart.js": "f0683ebb39daa27a06f6d501cc52f201",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
