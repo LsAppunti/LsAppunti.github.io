d20725d4f95b3f81c911d784340e0b7e82544c00
LsAppunti <lsappunti@gmail.com>
2025-02-17 17:14:07 +0100

    Remove of floating action button

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "34e5325d1ccba56f8e3348dd422bfe47",
+const RESOURCES = {"flutter_bootstrap.js": "d06e5fcce7b4da8ea8e32dbfd7d18939",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "c8e00fe9566c0df49eccc77e0d750dea",
+"main.dart.js": "bc2ff8467420b74178ffd596e5d7efc4",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "34e5325d1ccba56f8e3348dd422bfe47",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "6def58272a69f667ef1e9f80269f695b",
-"assets/fonts/MaterialIcons-Regular.otf": "fef4928b2ab5a30b855f38fd713f2366",
+"assets/fonts/MaterialIcons-Regular.otf": "295d8bfabb5c80adf1c21cc9d2abff41",
 "assets/assets/svg/Logo.svg": "de23c8f845ebbaf7c6e621286c5a74d9",
 "assets/assets/svg/LSAppunti.svg": "1fe3bde94a6dd875c6c85c79cc4fca05",
 "assets/assets/images/CaptureCamera.png": "dd839b44cb3d3cc4a4c21b9be42a9bfa",
