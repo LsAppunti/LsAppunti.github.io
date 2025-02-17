56ef9642310a5e73fe45c55b1cff5f1c27bb4558
LsAppunti <lsappunti@gmail.com>
2025-02-17 12:30:55 +0100

    Fix the bug of the remove of the base64Image when the back arrow is pressed in send_photo_page.dart, bug

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7fe050e8c0208a760ccd09ee47eb7ce5",
+const RESOURCES = {"flutter_bootstrap.js": "1303b2e156ebac472f62051cca5f17b1",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "3a1941e2d2a055697c717521c7922024",
+"main.dart.js": "1145f44559b9b6ff9a09729e2576e03d",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
