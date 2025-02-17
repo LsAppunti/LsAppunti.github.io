52b3c0013400e1636a837b767c67cb4741c42bca
LsAppunti <lsappunti@gmail.com>
2025-02-17 12:24:50 +0100

    Add the remove of the base64Image when the back arrow is pressed in send_photo_page.dart

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2593715373"
+    serviceWorkerVersion: "3536033550"
   }
 });
