ab4bf037a54af62a27d66b90405d58451b2618b4
LsAppunti <lsappunti@gmail.com>
2025-02-19 19:02:25 +0100

    Fix #2 for the color of the delete button in the user_page.dart

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "df2919412b847a2ac2f52cf8c04ab5a5",
+const RESOURCES = {"flutter_bootstrap.js": "5ac539b6f8f623e7e149f75b280a9b75",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
