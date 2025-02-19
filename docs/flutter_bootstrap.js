5983ebf4982cc1d90fffcf36def2f0f804f322ba
LsAppunti <lsappunti@gmail.com>
2025-02-19 18:51:39 +0100

    Fix #1 for the stop-working photo-taking and Fix #1 for the color of the delete button in the user_page.dart

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1525282151"
+    serviceWorkerVersion: "2008225657"
   }
 });
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f81679dd9665c5804179e21593b33e5e",
+const RESOURCES = {"flutter_bootstrap.js": "df2919412b847a2ac2f52cf8c04ab5a5",
 "version.json": "0c99bbdd478f38487bacd3051de23486",
 "favicon.ico": "8c43c96ef062b059552c9ee063be4459",
 "index.html": "94b649ad14bf07cc8c339724636c49a3",
 "/": "94b649ad14bf07cc8c339724636c49a3",
-"main.dart.js": "2e0b2b6ce34e42fb0222e03cf1f07e75",
+"main.dart.js": "a24839d32d0e101339a450fc917708f8",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "icons/apple-touch-icon.png": "0909de2679e845b0ac9f6e2aaca67c60",
 "icons/icon-192-maskable.png": "53221829be505105ae21ba209ed988c4",
